d9d087cac2359a3cad2d238bddb18713663cdb73
OSS-42 <eric.wursteisen@gmail.com>
2024-08-13 12:19:57 -0400

    start ex01

--- mobileModule00/ex00/lib/main.dart
+++ mobileModule00/ex00/lib/main.dart
@@ -1,3 +1,118 @@
+// import 'package:flutter/material.dart';
+// import 'package:english_words/english_words.dart';
+// import 'package:provider/provider.dart'; //pour le provider
+// import 'package:logger/logger.dart'; //pour logger en console plutot que print.
+
+// // to add a log in the console instead of print().
+// var logger = Logger(
+//   printer: PrettyPrinter(),
+// );
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return ChangeNotifierProvider(
+//       create: (context) => MyAppState(),
+//       child: MaterialApp(
+//         title: 'Mobile Module00 - ex00',
+//         theme: ThemeData(// This is the theme of your application.
+//           colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+//           useMaterial3: true,
+//         ),
+//         home: const MyHomePage(),
+//       ),
+//     );
+//   }
+// }
+
+// class MyAppState extends ChangeNotifier {
+//   var current = WordPair.random();
+// }
+
+// class MyHomePage extends StatelessWidget {
+//   const MyHomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // var appState = context.watch<MyAppState>();
+//     // var pair = appState.current;
+
+//     return Scaffold(
+//       body: Center(
+//         child: Column (
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             const SimpleText(),
+//             // BigCard(pair: pair),
+//             const SizedBox(height: 10),
+//             ElevatedButton (
+//               onPressed: () {
+//                 logger.i('Button pressed');
+//               },
+//               child: const Text('Click me'),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class SimpleText extends StatelessWidget {
+//   const SimpleText({
+//     super.key,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final theme = Theme.of(context);
+//     final style = theme.textTheme.displayMedium!.copyWith(
+//       color: theme.colorScheme.onPrimary,
+//     );
+
+//     return Card(
+//         color: theme.colorScheme.primary,
+//         child: Padding(
+//           padding: const EdgeInsets.all(20),
+//           child: Text('A simple text', style: style),
+//         ),
+//     );
+//   }
+// }
+
+// class BigCard extends StatelessWidget {
+//   const BigCard({
+//     super.key,
+//     required this.pair,
+//   });
+
+//   final WordPair pair;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final theme = Theme.of(context);
+//     final style = theme.textTheme.displayMedium!.copyWith(
+//       color: theme.colorScheme.onPrimary,
+//     );
+
+//     return Card(
+//       color: theme.colorScheme.primary,
+//       child: Padding(
+//         padding: const EdgeInsets.all(20),
+//         child: Text(pair.asPascalCase, style: style),
+//       ),
+//     );
+//   }
+// }
+//-----------------------------END OF EX00--------------------------------------
+
+//-----------------------------START OF EX01------------------------------------
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:provider/provider.dart'; //pour le provider
